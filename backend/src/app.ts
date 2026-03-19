5da7ae1a2cdefca2f09a891194e6e12d74c89610
Shivansh Tiwari <shivanshtiwari2014@gmail.com>
2026-03-19 12:14:56 +0530

    Task: setup the backend with db and redis

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,40 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import { rateLimit } from 'express-rate-limit';
+import pino from 'pino';
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(compression());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: 'draft-8', // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Too many requests from this IP, please try again later.',
+});
+
+export const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+});
+
+app.use(limiter);
 
 app.get('/', (req, res) => {
   res.json({
-    message:
-      'Unauthorized Access. Your access has been reported to the authorities.',
+    message: 'ACCESS BLOCKED',
   });
 });
 
